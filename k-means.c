b1eab2bcfd5681175cb2f08d990030f0cf02c16f
NickTheBit <nikosgklu@gmail.com>
2019-02-16 16:39:02 +0200

    Fixed seg fault

--- k-means.c
+++ k-means.c
@@ -13,11 +13,11 @@ int main(int argc,char **argv)
     int elementCounter = 0;   // Element counter for the file
     int clusterNum;           // Stores the cluster num
     double minDistance;       // This is utilised in the element assignment to clusters
-    int minCluster;           // Closest cluster at the moment
+    int minCluster;           // Closest cluster at the current centroid
     int biggestCluster = 0;   // Cluster containing the most data points, used for memory alocation
     int changesCommited;      // This will be used as a flag to end the algorythm when changes no longer happen
     int iterationCounter = 0; // This counts iterations
-    int changesCounter = 0;   // This is used to determine when to terminate the program
+    int ready[clusterNum];    // Ceeps record of centroid changes for termination
     int dbg = 0;              // Debug variable should be deleted printf("Debug %d\n",dbg++);
 
     // diagnosing file counting inputs
@@ -67,50 +67,48 @@ int main(int argc,char **argv)
         for (j = 0; j < dimentions; j++)
             fscanf(f, "%lf", &elements[i][j]);
 
-    
-    // Creating Centroid array
+    // Creating Centroid array and secondary array for termination check
     double **centroids;
+    double **exCentroids;
+
     centroids = calloc(clusterNum,sizeof(double));
-    for (i=0; i<clusterNum; i++)
+    exCentroids = calloc(clusterNum, sizeof(double));
+
+    for (i=0; i<clusterNum; i++) {
         centroids[i] = calloc(dimentions,sizeof(double));
+        exCentroids[i] = calloc(dimentions, sizeof(double));
+    }
 
     // Generating initial Centroids
     for (i = 0; i < clusterNum; i++)
-        for (j = 0; j < dimentions; j++)
+        for (j = 0; j < dimentions; j++) {
             centroids[i][j] = elements[rand() % elementCounter][j];
             // The line above may not correspond with k-means philoshophy
+        }
+        printf("%lf %lf %lf\n",centroids[0][0],centroids[0][1],centroids[0][2]);
+        printf("%lf %lf %lf\n",centroids[1][0],centroids[1][1],centroids[1][2]);
 
     int *clusterSize;
+    clusterSize = calloc(clusterNum,sizeof(int));
+    
     int **clusterSum;
+    clusterSum = calloc(clusterNum,sizeof(double));
+    for (i = 0; i < clusterNum; i++)
+        clusterSum[i] = calloc(dimentions,sizeof(double));
+
     int **clusters;
+    clusters = calloc(clusterNum,sizeof(int));
 
-    int **exClusters;
-    exClusters = calloc(clusterNum,sizeof(int));
+    // Starting loop for iterations
+    do {
 
-    int changed[clusterNum];                    // If changes are commited it will resume execution
-
-    do
-    {
-        // Reseting counters
-        biggestCluster = 0;
-        changesCommited = 1;
-
-        // Initialising clusters
-        printf("Debug %d\n",dbg++);
-        clusters = calloc(clusterNum,sizeof(int));
-
-        // Reseting cluster sizes
-        clusterSize = calloc(clusterNum,sizeof(int));
-
-        // Storing current cluster sums per dimention
-        clusterSum = calloc(clusterNum,sizeof(double));
-        for (i = 0; i < clusterNum; i++)
-            clusterSum[i] = calloc(dimentions,sizeof(double));
+        for (i=0; i<clusterNum; i++)
+            ready[i] = 0;
 
         // Asigning items to Centroids
         for (i = 0; i < elementCounter; i++)
         {
-            minDistance = 99999999; //change that
+            minDistance = 99999999; //change that... or not, its the biggest possible number...
             minCluster = 0;
             for (j = 0; j < clusterNum; j++)
             {
@@ -120,7 +118,6 @@ int main(int argc,char **argv)
                     tempDouble += (centroids[j][d] - elements[i][d])*(centroids[j][d] - elements[i][d]);
 
                 tempDouble = sqrt(tempDouble);
-
                 // finding the closest centroid
                 if (tempDouble <= minDistance)
                 {
@@ -136,65 +133,58 @@ int main(int argc,char **argv)
                     biggestCluster ++;
                 }
             clusters[minCluster][ clusterSize[minCluster] ] = i;
-
-            for (d = 0; d < dimentions; d++) {
+            for (d = 0; d < dimentions; d++)
                 clusterSum[minCluster][d] += elements[ clusters[minCluster][clusterSize[minCluster]] ][d];
-            }
-            
         }
-
         // Calculating new centroid
         for (i = 0; i < clusterNum; i++)
             for (j = 0; j < dimentions; j++)
                 centroids[i][j] = clusterSum[i][j] / (clusterSize[i]);
-        printf("New Centroid is : %lf %lf %lf \n",centroids[0][0],centroids[0][1],centroids[0][2]);
-        printf("New Centroid is : %lf %lf %lf \n",centroids[1][0],centroids[1][1],centroids[1][2]);
-        
+
         iterationCounter++;
-        printf("Iteration counter %d \n",iterationCounter);
 
         // Find if it should terminate
-        // Termination is wrong system needs to terminate when less than 2% of points change
-        if (iterationCounter == 1)
-            for(i=0; i<clusterNum; i++)
-                exClusters[i] = calloc(biggestCluster,sizeof(int));
-
-        for (i=0; i<clusterNum; i++)
-            for (j=0; j<clusterSize[i]; j++) {
-                if (exClusters[i][j] != clusters[i][j])
-                    changesCounter ++;
-                exClusters[i][j] = clusters[i][j];
+        for (i=0; i<clusterNum; i++) 
+            for (j=0; j<dimentions; j++) {
+                if (centroids[i][j] == exCentroids[i][j]) {
+                    ready[i] = 1;
+                } else {
+                    ready[i] = 0;
+                }
+                // Alocating centroids for next termination check
+                exCentroids[i][j] = centroids[i][j];
             }
-        printf("Changes %d\n",changesCounter);
-        
-        // if (changesCounter < elementCounter) //This is why it doesn't stop
-        if (iterationCounter >= 50) {
-            changesCommited = 0;
-        } else {
-            // In case it needs to be run again those need to reset
-            free(clusterSize);
-            free(clusterSum);
-            free(clusters);
-            printf("cleaning complete\n");
+
+        changesCommited = 0;
+        for (i=0; i<clusterNum; i++)
+            if (ready[i] == 0)
+                changesCommited = 1;
+
+        // In case it needs to be run again those need to reset
+        if (changesCommited == 1) {
+            for (i=0; i<clusterNum; i++) {
+                memset(clusters[i], 0,dimentions*sizeof(int));
+                memset(clusterSum[i], 0,dimentions*sizeof(double));
+            }
+            // Reseting cluster sizes
+            memset(clusterSize, 0,clusterNum*sizeof(int));
         }
     } while (changesCommited == 1);
 
-    
+    printf("Iterations : %d\n", iterationCounter);
     fclose(f);
-
-    printf("iterations : %d\n", iterationCounter);
     // Write results in a file
-    for (i = 0; i < clusterNum; i++)
+    for (i = 0; i<clusterNum; i++)
     {
         sprintf(fileName, "Cluster%d.csv", i);
         f = fopen(fileName, "w");
         for (j = 0; j < clusterSize[i]; j++)
         {
             for (d = 0; d < dimentions; d++)
-                fprintf(f, "%lf ", elements[j][d]);
+                fprintf(f, "%lf ", elements[clusters[i][j]][d]);
             fprintf(f, " \n");
         }
         fclose(f);
-    }
+    } 
     return 0;
 }
